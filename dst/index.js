b3b4eee56b161e1890b1b5da4bae3f65ee80fe7e
The Legendary Josh Wulf™ - As Seen on the Internet® <josh@magikcraft.io>
2017-08-28 22:59:17 +1000

    update event types

--- dst/index.js
+++ dst/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Events;
-(function (Events) {
-    Events["PlayerJoinedServer"] = "PlayerJoinedServer";
-    Events["PlayerQuitServer"] = "PlayerQuitServer";
-    Events["LoreSentToUser"] = "LoreSentToUser";
-    Events["PlayerCastSpell"] = "PlayerCastSpell";
-})(Events = exports.Events || (exports.Events = {}));
+var PlayerEventTypes;
+(function (PlayerEventTypes) {
+    PlayerEventTypes["PLAYAPP_USER_HEARTBEAT"] = "PLAYAPP_USER_HEARTBEAT";
+    PlayerEventTypes["PLAYER_JOINED_MINECRAFT_SERVER"] = "PLAYER_JOINED_MINECRAFT_SERVER";
+    PlayerEventTypes["PLAYER_QUIT_MINECRAFT_SERVER"] = "PLAYER_QUIT_MINECRAFT_SERVER";
+    PlayerEventTypes["PLAYER_UPSERT_LORE"] = "PLAYER_UPSERT_LORE";
+    PlayerEventTypes["PLAYER_CAST_SPELL"] = "PLAYER_CAST_SPELL";
+    PlayerEventTypes["PLAYER_WAS_AT"] = "PLAYER_WAS_AT";
+})(PlayerEventTypes = exports.PlayerEventTypes || (exports.PlayerEventTypes = {}));

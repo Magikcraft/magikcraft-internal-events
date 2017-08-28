db0daf1f41570ab9826736df8ecaa732e56653af
Tim Marwick <tim@magikcraft.io>
2017-08-28 23:36:37 +1000

    Update to use interface..

--- dst/index.js
+++ dst/index.js
@@ -1,11 +1,10 @@
 "use strict";
+// export enum PlayerEventTypes {
+//     PLAYAPPUSERHEARTBEAT,
+//     PLAYER_JOINED_MINECRAFT_SERVER,
+//     PLAYER_QUIT_MINECRAFT_SERVER,
+//     PLAYER_UPSERT_LORE,
+//     PLAYER_CAST_SPELL,
+//     PLAYER_WAS_AT,
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-var PlayerEventTypes;
-(function (PlayerEventTypes) {
-    PlayerEventTypes["PLAYAPP_USER_HEARTBEAT"] = "PLAYAPP_USER_HEARTBEAT";
-    PlayerEventTypes["PLAYER_JOINED_MINECRAFT_SERVER"] = "PLAYER_JOINED_MINECRAFT_SERVER";
-    PlayerEventTypes["PLAYER_QUIT_MINECRAFT_SERVER"] = "PLAYER_QUIT_MINECRAFT_SERVER";
-    PlayerEventTypes["PLAYER_UPSERT_LORE"] = "PLAYER_UPSERT_LORE";
-    PlayerEventTypes["PLAYER_CAST_SPELL"] = "PLAYER_CAST_SPELL";
-    PlayerEventTypes["PLAYER_WAS_AT"] = "PLAYER_WAS_AT";
-})(PlayerEventTypes = exports.PlayerEventTypes || (exports.PlayerEventTypes = {}));

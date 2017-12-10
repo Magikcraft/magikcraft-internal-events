82f21cae84f4598032773f9bf3e42d77fcb52faa
Joshua Wulf <sitapati@worldsankirtan.net>
2017-12-10 15:42:27 +1000

    Add PLAYER_LOCATION_HEARTBEAT

--- dst/index.js
+++ dst/index.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var PlayerEventTypes;
 (function (PlayerEventTypes) {
     PlayerEventTypes["PLAYAPP_USER_HEARTBEAT"] = "PLAYAPP_USER_HEARTBEAT";
-    PlayerEventTypes["PLAYER_JOINED_MINECRAFT_SERVER"] = "PLAYER_JOINED_MINECRAFT_SERVER";
-    PlayerEventTypes["PLAYER_QUIT_MINECRAFT_SERVER"] = "PLAYER_QUIT_MINECRAFT_SERVER";
-    PlayerEventTypes["PLAYER_UPSERT_LORE"] = "PLAYER_UPSERT_LORE";
     PlayerEventTypes["PLAYER_CAST_SPELL"] = "PLAYER_CAST_SPELL";
-    PlayerEventTypes["PLAYER_WAS_AT"] = "PLAYER_WAS_AT";
+    PlayerEventTypes["PLAYER_JOINED_MINECRAFT_SERVER"] = "PLAYER_JOINED_MINECRAFT_SERVER";
     PlayerEventTypes["PLAYER_JOINED_WORLD"] = "PLAYER_JOINED_WORLD";
+    PlayerEventTypes["PLAYER_LOCATION_HEARTBEAT"] = "PLAYER_LOCATION_HEARTBEAT";
+    PlayerEventTypes["PLAYER_QUIT_MINECRAFT_SERVER"] = "PLAYER_QUIT_MINECRAFT_SERVER";
     PlayerEventTypes["PLAYER_UPDATED_MINECRAFT_LOCALE"] = "PLAYER_UPDATED_MINECRAFT_LOCALE";
+    PlayerEventTypes["PLAYER_UPSERT_LORE"] = "PLAYER_UPSERT_LORE";
+    PlayerEventTypes["PLAYER_WAS_AT"] = "PLAYER_WAS_AT";
 })(PlayerEventTypes = exports.PlayerEventTypes || (exports.PlayerEventTypes = {}));
